030a55fcbcd1c18a83c0ad5c33698388e43f6bff
s18679 <s18679@pjwstk.edu.pl>
2021-01-29 00:08:00 +0100

    Cwiczenie 5
    Koniec tego dobrego, reszta jutro

---
+++ cw5/Wyklad3/DAL/IDbService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Wyklad3.Models;
+
+namespace cw3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
---
+++ cw5/Wyklad3/DTOs/Requests/EnrollStudentRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+public class EnrollStudentRequest
+{
+    [RegularExpression("^s[0-9]+$")]
+    public string IndexNumber{get; set;}
+
+    [Required(ErrorMessage = "Musisz podac imie")]
+    [MaxLength(10)]
+    public string FirstName{get; set;}
+
+    [Required(ErrorMessage = "Musisz podac nazwisko")]
+    [MaxLength(17)]
+    public string LastName{get; set;}
+
+    [Required]
+    public DateTime BirthDate{get; set;}
+
+    [Required]
+    public string Studies{get; set;}
+
+    [Required]
+    public int Semester { get; set; }
+}
